5e2af6281c3c176e5d7a53a5b7d4fc0ed09def5d
Ntouch <ntouch.development@gmail.com>
2023-03-08 12:27:18 +0100

    création de la table Band dans le cours

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Band
 
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("<h1>Hello Django !</h1>")
+    bands = Band.objects.all()
+    return HttpResponse("<h1>Hello Django !</h1><p>Mes groupes préférés sont:</p>"
+                        "<ul>"
+                        f"  <li>{bands[0].name}</li>"
+                        f"  <li>{bands[1].name}</li>"
+                        f"  <li>{bands[2].name}</li>"
+                        f"</ul>")
 
 
 def about(request):
