1583d99def02b8d02333d9cdc8a27147de56543b
Ntouch <ntouch.development@gmail.com>
2023-03-08 02:06:13 +0100

    add two more pages : contact-us and listings

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def hello(request):
     return HttpResponse("<h1>Hello Django !</h1>")
+
+
+def about(request):
+    return HttpResponse("<h1>Info</h1><p>We love merch</p>")
+
+
+def listings(request):
+    return HttpResponse("<h1>LISTINGS</h1><p>Voilà nos produits !</p>")
+
+
+def contact(request):
+    return HttpResponse("<h1>Contact us</h1><p>Formulaire de contact</p>")
