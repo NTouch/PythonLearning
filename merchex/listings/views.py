af5c4a3ddb1d10dbf130930b2ad3a90af7a82d6a
Ntouch <ntouch.development@gmail.com>
2023-03-08 13:37:04 +0100

    views new render

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from listings.models import Band, Listing
 
 
@@ -10,19 +9,13 @@ def hello(request):
 
 
 def about(request):
-    return HttpResponse("<h1>Info</h1><p>We love merch</p>")
+    return render(request, "listings/about.html")
 
 
 def listings(request):
     listing = Listing.objects.all()
-    return HttpResponse("<h1>LISTINGS</h1><p>Voilà nos produits :</p>"
-                        "<ul>"
-                        f"  <li>{listing[0].title}</li>"
-                        f"  <li>{listing[1].title}</li>"
-                        f"  <li>{listing[2].title}</li>"
-                        f"  <li>{listing[3].title}</li>"
-                        "</ul>")
+    return render(request, "listings/listings.html", {"listing": listing})
 
 
 def contact(request):
-    return HttpResponse("<h1>Contact us</h1><p>Formulaire de contact</p>")
+    return render(request, "listings/contact.html")
