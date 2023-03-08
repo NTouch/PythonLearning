58c42eaecebf7c7b8b147435466cff33a92dd0d2
Ntouch <ntouch.development@gmail.com>
2023-03-08 13:35:05 +0100

    exo gabarit, gabarit variables et render

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -20,7 +20,7 @@ from listings import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("hello/", views.hello),
-    path("about-us/", views.about),
+    path("about/", views.about),
     path("listings/", views.listings),
-    path("contact-us/", views.contact)
+    path("contact/", views.contact)
 ]
